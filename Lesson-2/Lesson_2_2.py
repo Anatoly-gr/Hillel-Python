62dd72c5e5eed46d968c3f4af40d220c0aa7ce6b
Anatoly-gr <anatoly.gr2012@gmail.com>
2021-03-28 17:46:48 +0300

    Lesson 2.2

--- Lesson-2/Lesson_2_2.py
+++ Lesson-2/Lesson_2_2.py
@@ -1,2 +1,3 @@
-numbers = input('Please enter an integer number: ')
-print(numbers + 1)
+numbers = int(input('Please enter an integer number: '))
+print('The next number for the number ', numbers, 'is', numbers + 1, '\b.')
+print('The previous  number for the number', numbers, 'is', numbers - 1, '\b.')
