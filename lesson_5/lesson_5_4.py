6b002b2ba2e09f6e30f156694a24ae0772a05fdb
anatoly <anayoly.gr2012@gmail.com>
2021-04-11 08:35:57 +0300

    lesson_5_4.py

--- lesson_5/lesson_5_4.py
+++ lesson_5/lesson_5_4.py
@@ -4,8 +4,14 @@ from random import randint
 
 num_list = [randint(10, 100) for x in range(randint(10, 20))]
 print(num_list)
-# num_list = [num for num in num_list if num % 2 == 0: num = 0]
+new_num_list = []
+
 for num in num_list:
     if num % 2 == 0:
-        print(num)
-print(num_list)
+        num = 0
+        new_num_list.append(num)
+    else:
+        new_num_list.append(num)
+# print(num_list)
+print(new_num_list)
+print('Количество четных чисел ', new_num_list.count(0))
