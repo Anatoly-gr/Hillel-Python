126c6b6a4f54df57ec11eb5e148d8d38f837123f
anatoly <anayoly.gr2012@gmail.com>
2021-04-12 09:29:08 +0300

    lesson_6_1.py

--- lesson_6/lesson_6_1.py
+++ lesson_6/lesson_6_1.py
@@ -1,20 +1,19 @@
 # 1.Дано два кортежа, напишите функцию которая соединит их в один dict:
 #
 #    Input:
-#
 # coin = ('Bitcoin', 'Ether', 'Ripple', 'Litecoin')
 # code = ('BTC', 'ETH', 'XRP', 'LTC')
 #
 #    Output:
-#
 # {'Bitcoin': 'BTC', 'Ether': 'ETH', 'Ripple': 'XRP', 'Litecoin': 'LTC'}
-def uplink(**kwargs):
-    new_dict = {x: y for x, y in zip(coin, codes)}
-    print(some_dict)
+
+def uplink(*first):
+    k = list(first[0])
+    v = list(first[1])
+    new_dict = {x: y for x, y in zip(k, v)}
+    return new_dict
 
 
 coin = ('Bitcoin', 'Ether', 'Ripple', 'Litecoin')
 codes = ('BTC', 'ETH', 'XRP', 'LTC')
-
-some_dict = {x: y for x, y in zip(coin, codes)}
-print(some_dict)
+print(uplink(coin, codes))
