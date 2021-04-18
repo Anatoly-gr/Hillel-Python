c6bcdf0f2db09453e9d23ad19c5d4fefb3b2724a
anatoly <anatoly.gr2012@gmail.com>
2021-04-18 10:50:51 +0300

    lesson_7_2.py

--- lesson_7/lesson_7_2.py
+++ lesson_7/lesson_7_2.py
@@ -4,26 +4,21 @@
 # Программа должна вывести температуру в трех шкалах температур - Цельсий, Фарренгейт, Кельвин.
 
 type_temperature = input('enter type of temperature: "C", "K", "F" ')
-val_temperature = int(input('Input number of temperature: '))
-temp_dict = {'k': val_temperature, 'c': val_temperature, 'f': val_temperature}
-# print(temp_dict)
-if type_temperature == 'k' and val_temperature < 0:
-    print("значение температуры не может быть меньше нуля")
+val_temperature = float(input('Input number of temperature: '))
 
-if type_temperature == 'k':
-    temp_dict = {'k': val_temperature, 'c': val_temperature - 273.15, 'f': val_temperature - 459.67}
-    print('Temperature C: ', temp_dict['c'])
-    print('Temperature K: ', temp_dict['k'])
-    print('Temperature F: ', temp_dict['f'])
-if type_temperature == 'c':
-    temp_dict = {'k': val_temperature + 273.15, 'c': val_temperature,
-                 'f': val_temperature * 9 / 5 + 32}
-    print('Temperature C: ', temp_dict['c'])
-    print('Temperature K: ', temp_dict['k'])
-    print('Temperature F: ', temp_dict['f'])
-if type_temperature == 'f':
-    temp_dict = {'k': (val_temperature - 32) * 5 / 9 + 273.15,
-                 'c': (val_temperature - 32) * 5 / 9, 'f': val_temperature}
-    print('Temperature C: ', temp_dict['c'])
-    print('Temperature K: ', temp_dict['k'])
-    print('Temperature F: ', temp_dict['f'])
+if type_temperature == 'k' and val_temperature >= 0:
+    print('Temperature C: ', val_temperature - 273.15)
+    print('Temperature K: ', val_temperature)
+    print('Temperature F: ', val_temperature - 459.67)
+
+if type_temperature == 'c' and val_temperature + 273.15 >= 0:
+    print('Temperature C: ', val_temperature)
+    print('Temperature K: ', val_temperature + 273.15)
+    print('Temperature F: ', val_temperature * 9 / 5 + 32)
+
+if type_temperature == 'f' and val_temperature + 459.67 >= 0:
+    print('Temperature C: ', (val_temperature - 32) * 5 / 9)
+    print('Temperature K: ', (val_temperature - 32) * 5 / 9 + 273.15)
+    print('Temperature F: ', val_temperature)
+else:
+    print('The temperature lower than absolute "zero"')
