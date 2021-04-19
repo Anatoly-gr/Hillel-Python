b85dc4bbaaf7fc49a7830d65378bea750adbfc7e
anatoly <anatoly.gr2012@gmail.com>
2021-04-19 09:06:48 +0300

    lesson_7_3.py

--- lesson_7/lesson_7_3.py
+++ lesson_7/lesson_7_3.py
@@ -3,15 +3,16 @@ import urllib.request
 import json
 from datetime import datetime
 
-url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=odesa&cnt=5&units=metric&appid=f9ada9efec6a3934dad5f30068fdcbb8'
+url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=odesa&cnt=5&units=metric&appid=f9ada9efec6a3934dad5f30068fdcbb8"
 request = urllib.request.urlopen(url)
 data = json.load(request)
 print(data)
-for
 new_list = data['list']
-for i in [0,1,2,3,4]:
-    new_dict = new_list[i]
-    print(new_dict['temp']['day'])
-    dt_data = new_dict['dt']
-    print(datetime.fromtimestamp(dt_data).strftime("%d-%m-%Y"))
-
+with open('19-04-2021-Odessa-5-days-weather-forecast.txt', 'r+') as file:
+    for i in [0, 1, 2, 3, 4]:
+        new_dict = new_list[i]
+        dt_data = new_dict['dt']
+        print(f'{datetime.fromtimestamp(dt_data).strftime("%d-%m-%Y")}, {new_dict["temp"]["day"]}, '
+              f'{new_dict["temp"]["night"]}, {new_dict["feels_like"]["day"]}')
+        file.write(f'\n {datetime.fromtimestamp(dt_data).strftime("%d-%m-%Y")}, {new_dict["temp"]["day"]}, '
+                   f'{new_dict["temp"]["night"]}, {new_dict["feels_like"]["day"]}, \n')
