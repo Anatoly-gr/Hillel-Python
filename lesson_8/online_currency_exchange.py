e0dd3c3ca7106561a28f36a2e2cde0979309bb7c
anatoly <anatoly.gr2012@gmail.com>
2021-04-25 10:58:10 +0300

    currency_exchange

--- lesson_8/online_currency_exchange.py
+++ lesson_8/online_currency_exchange.py
@@ -4,23 +4,31 @@ import json
 import urllib.request
 import requests
 
-currency_from = str(input('Введите валюту from: '))
-currency_to = str(input('Введите валюту для конвертации to: '))
+currency_from = str(input('Введите тип валюты from: '))
+currency_to = str(input('Введите тип валюты для конвертации to: '))
 amount = float(input('введите сумму для конвертации: '))
 start_date = str(input('Введите дату: '))
-
 start_date = datetime.strptime(start_date, '%Y-%m-%d')
 today = datetime.today()
-print(f"'date',   'from',   'to', 'amount',\t 'rate', \t'result'")
-while start_date < today:
-    url = "https://api.exchangerate.host/convert"
-    payload = {'from': currency_from, 'to': currency_to,    'amount': amount, 'date': start_date}
+if start_date > today:
+    start_date = today
+with open('symbols.json', 'r+') as file:
+    cur_dict = (json.load(file))
+    currency_to = currency_to.upper()
+    currency_from = currency_from.upper()
+    if currency_from in cur_dict['symbols'] and currency_to in cur_dict['symbols']:
+        # start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        print(f"'date',   'from',   'to', 'amount',\t 'rate', \t'result'")
+        while start_date <= today:
+            url = "https://api.exchangerate.host/convert"
+            payload = {'from': currency_from, 'to': currency_to, 'amount': amount, 'date': start_date}
 
-    r = requests.get(url, params=payload)
-    request = urllib.request.urlopen(r.url)
-    data = json.load(request)
-    # print(data)
-    print(f"{start_date.strftime('%d-%m-%Y')}, {data['query']['from']}, '\t'{data['query']['to']}, "
-          f" {data['query']['amount']}, \t {data['info']['rate']}, {data['result']}")
-    start_date += timedelta(days=1)
-
+            r = requests.get(url, params=payload)
+            request = urllib.request.urlopen(r.url)
+            data = json.load(request)
+            # print(data)
+            print(f"{start_date.strftime('%d-%m-%Y')}, {data['query']['from']}, '\t'{data['query']['to']}, "
+                  f" {data['query']['amount']}, \t {data['info']['rate']}, {data['result']}")
+            start_date += timedelta(days=1)
+    else:
+        print('Wrong type of currency')
