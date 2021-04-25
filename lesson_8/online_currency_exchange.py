be47936e8ece8d3fa0a7c8e01b402147c997c831
anatoly <anatoly.gr2012@gmail.com>
2021-04-25 15:30:41 +0300

    currency_exchange

--- lesson_8/online_currency_exchange.py
+++ lesson_8/online_currency_exchange.py
@@ -1,49 +1,54 @@
 import argparse
-from datetime import timedelta
 from datetime import datetime
+from datetime import timedelta
 import json
 import urllib.request
+
 import requests
 
 
-currency_from = str(input('Введите тип валюты from: '))
-currency_to = str(input('Введите тип валюты для конвертации to: '))
-amount = float(input('введите сумму для конвертации: '))
-start_day = str(input('Введите дату в формате YYYY-mm-dd: '))
-today = datetime.today()
-
-try:
-    start_day = datetime.strptime(start_day, '%Y-%m-%d')
-    if start_day > datetime.today():
+def main():
+    currency_from = args.cur_f
+    currency_to = args.cur_to
+    amount = args.amnt
+    start_day = args.start_day
+    today = datetime.today()
+    try:
+        start_day = datetime.strptime(start_day, '%Y-%m-%d')
+        if start_day > datetime.today():
+            start_day = today
+    except ValueError as err:
+        print('You entered is incorrect date', err)
         start_day = today
-except ValueError as err:
-    print('You entered is incorrect date', err)
-    start_day = today
+    # print(currency_from, currency_to, amount, start_day)
+    with open('symbols.json', 'r+') as file:
+        cur_dict = json.load(file)
+        currency_to = currency_to.upper()
+        currency_from = currency_from.upper()
+        if currency_from in cur_dict['symbols'] and currency_to in cur_dict['symbols']:
+            print(f"'date',   'from', 'to', 'amount',\t'rate',\t\t'result'")
+            while start_day <= today:
+                url = "https://api.exchangerate.host/convert"
+                payload = {'from': currency_from, 'to': currency_to, 'amount': amount, 'date': start_day}
 
-with open('symbols.json', 'r+') as file:
-    cur_dict = (json.load(file))
-    currency_to = currency_to.upper()
-    currency_from = currency_from.upper()
-    if currency_from in cur_dict['symbols'] and currency_to in cur_dict['symbols']:
-        print(f"'date',   'from',   'to', 'amount',\t 'rate', \t'result'")
-        while start_day <= today:
-            url = "https://api.exchangerate.host/convert"
-            payload = {'from': currency_from, 'to': currency_to, 'amount': amount, 'date': start_day}
+                r = requests.get(url, params=payload)
+                request = urllib.request.urlopen(r.url)
+                data = json.load(request)
+                # print(data)
+                print(f"{start_day.strftime('%d-%m-%Y')}, {data['query']['from']},  {data['query']['to']}, "
+                      f" {data['query']['amount']}, \t\t {data['info']['rate']}, \t{data['result']}")
+                start_day += timedelta(days=1)
+        else:
+            print('Wrong type of currency')
+    return
 
-            r = requests.get(url, params=payload)
-            request = urllib.request.urlopen(r.url)
-            data = json.load(request)
-            print(f"{start_day.strftime('%d-%m-%Y')}, {data['query']['from']}, '\t'{data['query']['to']}, "
-                  f" {data['query']['amount']}, \t {data['info']['rate']}, {data['result']}")
-            start_day += timedelta(days=1)
-    else:
-        print('Wrong type of currency')
+if __name__ == '__main__':
+    t = datetime.today().strftime('%Y-%m-%d')
+    parser = argparse.ArgumentParser(description='Currency exchange')
+    parser.add_argument('cur_f')
+    parser.add_argument('cur_to')
+    parser.add_argument('amnt')
+    parser.add_argument('--start_day', default=t, type=str)
+    args = parser.parse_args()
 
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser(description='Currency exchange')
-#     parser.add_argument('cur_f')
-#     parser.add_argument('cur_to')
-#     parser.add_argument('amnt')
-#     parser.add_argument('strt_d')
-#
-#     args = parser.parse_args()
+main()
