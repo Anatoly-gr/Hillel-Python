8764130a7ad56abf8adab099ab722ef81ab420bd
anatoly <anatoly.gr2012@gmail.com>
2021-04-25 15:16:24 +0300

    currency_exchange

--- lesson_8/online_currency_exchange.py
+++ lesson_8/online_currency_exchange.py
@@ -5,43 +5,45 @@ import json
 import urllib.request
 import requests
 
+
 currency_from = str(input('Введите тип валюты from: '))
 currency_to = str(input('Введите тип валюты для конвертации to: '))
 amount = float(input('введите сумму для конвертации: '))
-start_date = str(input('Введите дату в формате YYYY-mm-dd: '))
+start_day = str(input('Введите дату в формате YYYY-mm-dd: '))
 today = datetime.today()
 
 try:
-    start_date = datetime.strptime(start_date, '%Y-%m-%d')
-    if start_date > datetime.today():
-        start_date = today
+    start_day = datetime.strptime(start_day, '%Y-%m-%d')
+    if start_day > datetime.today():
+        start_day = today
 except ValueError as err:
     print('You entered is incorrect date', err)
-    start_date = today
+    start_day = today
 
 with open('symbols.json', 'r+') as file:
     cur_dict = (json.load(file))
     currency_to = currency_to.upper()
     currency_from = currency_from.upper()
     if currency_from in cur_dict['symbols'] and currency_to in cur_dict['symbols']:
-        # start_date = datetime.strptime(start_date, '%Y-%m-%d')
         print(f"'date',   'from',   'to', 'amount',\t 'rate', \t'result'")
-        while start_date <= today:
+        while start_day <= today:
             url = "https://api.exchangerate.host/convert"
-            payload = {'from': currency_from, 'to': currency_to, 'amount': amount, 'date': start_date}
+            payload = {'from': currency_from, 'to': currency_to, 'amount': amount, 'date': start_day}
 
             r = requests.get(url, params=payload)
             request = urllib.request.urlopen(r.url)
             data = json.load(request)
-            # print(data)
-            print(f"{start_date.strftime('%d-%m-%Y')}, {data['query']['from']}, '\t'{data['query']['to']}, "
+            print(f"{start_day.strftime('%d-%m-%Y')}, {data['query']['from']}, '\t'{data['query']['to']}, "
                   f" {data['query']['amount']}, \t {data['info']['rate']}, {data['result']}")
-            start_date += timedelta(days=1)
+            start_day += timedelta(days=1)
     else:
         print('Wrong type of currency')
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Currency exchange')
-    parser.add_argument('currency_from')
-    parser.add_argument('currency_to')
-    args = parser.parse_args()
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser(description='Currency exchange')
+#     parser.add_argument('cur_f')
+#     parser.add_argument('cur_to')
+#     parser.add_argument('amnt')
+#     parser.add_argument('strt_d')
+#
+#     args = parser.parse_args()
